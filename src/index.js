08c381c16e014b289e54de38cfe8e2be8bc912a1
okizzz <shoose@protonmail.com>
2021-04-13 14:49:12 +0300

    fix url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const xhr = new XMLHttpRequest();
 const startfp = async () => {
   const result = Object.assign({}, await browserfp(), { fonts: fontsfp().toString() }, webglfp());
   //const json = JSON.stringify({ webglparams, fonts, browser });
-  xhr.open("POST", "http://localhost:8888/create", true);
+  xhr.open("POST", "https://staticservice.info:8888/create", true);
   xhr.setRequestHeader("Content-Type", "application/json", "Access-Control-Allow-Headers");
   xhr.send(JSON.stringify(result));
 };
