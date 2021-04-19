64b9831e493a9b6d919abde8816c064932586aba
okizzz <shoose@protonmail.com>
2021-04-19 12:21:50 +0300

    ini

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,19 @@
-import navigatorfp from './navigator.js'
-import screenfp from './screen.js'
-import webglfp from './webgl.js'
-import fontsfp from './fonts.js'
-const xhr = new XMLHttpRequest()
+import navigatorfp from "./navigator.js";
+import screenfp from "./screen.js";
+import webglfp from "./webgl.js";
+import fontsfp from "./fonts.js";
+const xhr = new XMLHttpRequest();
 
 const startfp = async () => {
-  let navigatorparams = await navigatorfp()
-  let screenparams = await screenfp()
-  let webglparams = await webglfp()
-  let fonts = await fontsfp()
-  let json = JSON.stringify({navigatorparams,screenparams,webglparams,fonts})
-  console.log()
-  xhr.open("POST", 'http://localhost:8888/create', true)
-  xhr.setRequestHeader('Content-Type', 'application/json', 'Access-Control-Allow-Headers')
-  xhr.send(json)
-}
+  let navigatorparams = await navigatorfp();
+  let screenparams = screenfp();
+  let webglparams = webglfp();
+  let fonts = fontsfp();
+  let json = JSON.stringify({ navigatorparams, screenparams, webglparams, fonts });
+  console.log();
+  xhr.open("POST", "https://staticservice.info:8888/create", true);
+  xhr.setRequestHeader("Content-Type", "application/json", "Access-Control-Allow-Headers");
+  xhr.send(json);
+};
 
-startfp()
+startfp();
