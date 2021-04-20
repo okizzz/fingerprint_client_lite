c76662a74a8747ae83820b7f8d13c5f8bb312140
okizzz <shoose@protonmail.com>
2021-04-20 17:11:43 +0300

    fix webgl ex_1

--- src/modules/webgl/webgl.js
+++ src/modules/webgl/webgl.js
@@ -42,8 +42,8 @@ const webglTwo = () => {
   const canvas2 = document.createElement("canvas");
   const gl2 = canvas2.getContext("webgl2") || canvas2.getContext("experimental-webgl2");
   if (gl2) {
-    let EXTENNSION_ONE = gl2.getSupportedExtensions().toString();
-    return { EXTENNSION_ONE };
+    let EXTENNSION_TWO = gl2.getSupportedExtensions().toString();
+    return { EXTENNSION_TWO };
   }
   return null;
 };
